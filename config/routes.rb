2d4db558407e823e6e2243a606d0f1298517b5c6
Revel Roestorf <revel.roestorf@gmail.com>
2018-04-27 20:01:46 +1000

    added: name columns to users model, navbar to app controller, seeds, relationships, migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+
   resources :cars
+
+  devise_for :users
+
   get 'home/home'
 
   get 'home/how_it_works'
 
   get 'home/pricing'
 
-  devise_for :users
-  
   root to: "home#home"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{email: 'revsails@gmail', password: 'sparkle', firstname: 'revel', lastname: 'roestorf'},
+  {email: 'jane@gmail', password: 'sparkle', firstname: 'jane', lastname: 'jensen'},
+  {email: 'zac@gmail', password: 'sparkle', firstname: 'zac', lastname: 'chop'},
+  {email: 'holly@gmail', password: 'sparkle', firstname: 'holly', lastname: 'may'},
+  {email: 'ray@gmail', password: 'sparkle', firstname: 'ray', lastname: 'charles'},
+  {email: 'jay@gmail', password: 'sparkle', firstname: 'jay', lastname: 'bart'},
+  {email: 'PAT@gmail', password: 'sparkle', firstname: 'PAT', lastname: 'SMITH'},
+  {email: 'Don@gmail', password: 'sparkle', firstname: 'Don', lastname: 'Jamin'},
+  {email: 'butcH@gmail', password: 'sparkle', firstname: 'butcH', lastname: 'aspeN'}
+  ])
+
+Car.create([{user_id: 1, make: 'nissan', model: 'navara', year: '2014', location: '168 hedges ave, mermaid beach, qld', price_per_day: 40, price_per_km: 0.3},
+  {user_id: 1, make: 'aston martin', model: 'db 9', year: '2018', location: '168 hedges ave, mermaid beach, qld', price_per_day: 80, price_per_km: 0.4},
+  {user_id: 2, make: 'toyota', model: 'corolla', year: '2016', location: '1 breakfast creek rd, newstead, qld', price_per_day: 25, price_per_km: 0.18},
+  {user_id: 3, make: 'tesla', model: 'model s', year: '2015', location: '158 robinson st, fortitude valley, qld', price_per_day: 70, price_per_km: 0.2},
+  {user_id: 4, make: 'tesla', model: 'model x', year: '2017', location: '327 given terrace, paddington, qld', price_per_day: 60, price_per_km: 0.22},
+  {user_id: 5, make: 'tesla', model: 'model 3', year: '2018', location: '76 the esplanade, burleigh heads, qld', price_per_day: 30, price_per_km: 0.15},
+  {user_id: 6, make: 'porche', model: 'gt3', year: '2012', location: '30 pinnacle ct, robina, qld', price_per_day: 60, price_per_km: 0.35},
+  {user_id: 7, make: 'HYUNDAI', model: 'GETZ', year: '2014', location: '26 lather st, southport, qld', price_per_day: 20, price_per_km: 0.2},
+  {user_id: 8, make: 'Vw', model: 'Amarok', year: '2016', location: '8 beard st, auchenflower, qld', price_per_day: 45, price_per_km: 0.35},
+  {user_id: 9, make: 'bmW', model: '330', year: '2016', location: '2 booran st, hope island, qld', price_per_day: 35, price_per_km: 0.25}
+  ])
