caf20d1d2ec0b04481f1e896c2279cb74bc3a599
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-01 22:46:14 +1000

    Replaced non-active bookings in views with link to previous trips

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,44 +11,49 @@ class BookingsController < ApplicationController
   end
 
   def owner_bookings
-    @active_bookings_array = []
-    @non_active_bookings_array = []
+    @guest = false
+    @active = true
+    @bookings = []
     current_user.cars.each do |car|
-      car.bookings.each do |booking|
-        if booking.active == true
-          if params[:odo_start]
-            booking.update(odo_start: params[:odo_start])
-          end
-          if params[:odo_end]
-            booking.update(odo_end: params[:odo_end])
-          end
-          @active_bookings_array.push(booking)
-        else
-          @non_active_bookings_array.push(booking)
+      car.bookings.where(active: true).each do |booking|
+        if params[:odo_start]
+          booking.update(odo_start: params[:odo_start])
         end
+        if params[:odo_end]
+          booking.update(odo_end: params[:odo_end])
+        end
+        @bookings.push(booking)
       end
-    end
-    if @active_bookings_array || @non_active_bookings_array
-      @guest = false
     end
   end
 
   def previous_bookings
-
+    @guest = false
+    @active = false
+    @bookings = []
+    current_user.cars.each do |car|
+      car.bookings.where(active: false).each do |booking|
+        @bookings.push(booking)
+      end
+    end
+    render(bookings_owner_bookings_path)
   end
 
   def guest_trips
-    @active_trips_array = []
-    @non_active_trips_array = []
-    current_user.bookings.each do |booking|
-      if booking.active
-        @active_trips_array.push(booking)
-      else
-        @non_active_trips_array.push(booking)
-      end
+    @guest = true
+    @active = true
+    @bookings = []
+    current_user.bookings.where(active: true).each do |booking|
+      @bookings.push(booking)
     end
-    if @active_trips_array || @non_active_trips_array
-      @guest = true
+  end
+
+  def previous_trips
+    @guest = true
+    @active = false
+    @bookings = []
+    car.bookings.where(active: false).each do |booking|
+        @bookings.push(booking)
     end
   end
 
