d30020a144ef460352515c987f538f012da1741b
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-04 23:52:54 +1000

    port number uncommented in database yml file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  publishable_key: ENV['PUBLISHABLE_KEY'],
+  secret_key: ENV['SECRET_KEY']
 }
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# So we can use the key - access the ENV array
+Stripe.api_key = ENV['SECRET_KEY']
+# So Stripe can access the key
