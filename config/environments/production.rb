fd46d77ce964897f45926dc322751cbc6e14d827
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-05 10:20:54 +1000

    Added smtp setting to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,19 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+
+
+    ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USER'],
+    :password => ENV['SENDGRID_PASS'],
+    :domain => 'coderacademy.edu.au',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
