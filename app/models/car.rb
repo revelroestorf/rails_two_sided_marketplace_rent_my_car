968b1b1ca225c5d7b0d1ea9c5cd24bc679543233
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-10 10:27:40 +1000

    reset db - image urls now set - made comments in car model for seeding

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,11 +7,13 @@ class Car < ApplicationRecord
 
   geocoded_by :full_address
 
-  # after_validation :geocode
+  # Before seed, comment out:
+  after_validation :geocode
+  # then uncomment and run: $ rake geocode:all CLASS=Car in the terminal
 
-  # rake geocode:all CLASS=YourModel
-
+  # Before seed, comment out:
   mount_uploader :image, CarImageUploader
+  # then uncomment 
 
 
 end
